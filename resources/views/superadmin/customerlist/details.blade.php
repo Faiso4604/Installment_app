eeb3ea14f8cd3e727fd4f304043659531612c9aa
Faisoo4604 <faisoo4604@gmail.com>
2023-10-28 13:21:11 +0500

    commit message

--- resources/views/superadmin/customerlist/details.blade.php
+++ resources/views/superadmin/customerlist/details.blade.php
@@ -57,31 +57,83 @@
                             {{-- Product Details --}}
                             @foreach ($items as $item)
                                 <div class="row">
-                                    <div class="col-md-6 text-right">
-                                        <div class="mb-2"><strong>Item Name: </strong>{{ $item->item_name }}
+                                    <div class="col-md-6">
+                                        <div class="row border p-2">
+                                            <div class="col-md-6 text-right">
+                                                <div class="mb-2"><strong>Item Name: </strong>{{ $item->item_name }}
+                                                </div>
+                                                <div class="mb-2"><strong>Actual Price: </strong>{{ $item->item_price }}
+                                                </div>
+                                                <div class="mb-2"><strong>Down Payment:
+                                                    </strong>{{ $item->down_payment }}
+                                                </div>
+                                                <div class="mb-2"><strong>Remaining Balance:
+                                                    </strong>{{ $item->balance }}</div>
+                                            </div>
+
+                                            <div class="col-md-6 text-right">
+                                                <div class="mb-2"><strong>Profit: </strong>{{ $item->profit }}
+                                                </div>
+                                                <div class="mb-2"><strong>Due: </strong>{{ $item->total_due_amount }}
+                                                </div>
+                                                <div class="mb-2"><strong>Per Month:
+                                                    </strong>{{ $item->per_month }}</div>
+                                                <div class="mb-2"><strong>Total:
+                                                    </strong>{{ $item->total_amount }}</div>
+                                            </div>
                                         </div>
-                                        <div class="mb-2"><strong>Actual Price: </strong>{{ $item->item_price }}
-                                        </div>
-                                        <div class="mb-2"><strong>Down Payment:
-                                            </strong>{{ $item->down_payment }}
-                                        </div>
-                                        <div class="mb-2"><strong>Remaining Balance:
-                                            </strong>{{ $item->balance }}</div>
                                     </div>
 
-                                    <div class="col-md-6 text-right">
-                                        <div class="mb-2"><strong>Profit: </strong>{{ $item->profit }}
-                                        </div>
-                                        <div class="mb-2"><strong>Due: </strong>{{ $item->total_due_amount }}
-                                        </div>
-                                        <div class="mb-2"><strong>Per Month:
-                                            </strong>{{ $item->per_month }}</div>
-                                        <div class="mb-2"><strong>Total:
-                                            </strong>{{ $item->total_amount }}</div>
+                                    <div class="col-md-6 border p-2">
+                                        <form class="form-inline">
+                                            <form class="form-group">
+                                                <div class="col-md-6 m-auto text-center mt-4">
+                                                    <input type="text" class="form-control text-center" id="receivingAmount"
+                                                        placeholder="Enter the receiving amount">
+                                                </div>
+                                                <div class="row text">
+                                                    <div class="col-md-12 m-2 text-center">
+                                                        <button type="submit" class="btn btn-primary">Add installment</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+
+                                        </form>
                                     </div>
                                 </div>
                             @endforeach
                         </div>
+
+                        @if (count($items) > 0)
+                        <div class="mx-3">
+                            <table class="table table-bordered table-responsive">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Installment</th>
+                                        <th scope="col">Remaining</th>
+                                        <th scope="col">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th></th>
+                                        <td></td>
+                                        <td></td>
+                                        <td>
+                                            <a href="{{ route('customer.destroy', $customer) }}" class="btn btn-danger">
+                                                <i data-feather="trash-2"></i> Delete
+                                            </a>
+                                        </td>
+                                    </tr>
+                            </table>
+                        </div>
+                    @else
+                        <div class="alert alert-info m-4 text-center">No record found</div>
+                    @endif
+                        {{-- Istallments table --}}
+
+
                         <div class="row justify-content-end mx-3">
                             <div class="col-auto">
                                 {{-- {{ $items->links('vendor.pagination.bootstrap-5') }} --}}
